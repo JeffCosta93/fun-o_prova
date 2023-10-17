469a7151300ab38db7853d1ca7cbcc1feef11f75
JeffCosta93 <142415628+JeffCosta93@users.noreply.github.com>
2023-10-16 22:22:49 -0300

    Add files via upload

---
+++ Função_prov.py
@@ -0,0 +1,21 @@
+def maior_numero(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+num1 = 10
+num2 = 7
+resultado = maior_numero(num1, num2)
+print(f"O maior número entre {num1} e {num2} é: {resultado}")
+
+def maior_numero(a, b):
+    return max(a, b)
+
+num1 = 10
+num2 = 7
+
+resultado = maior_numero(num1, num2)
+print(f"O maior número entre {num1} e {num2} é: {resultado}")
+
+
